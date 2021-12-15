8c97bdc2c6b01d257744b130a165bdf1915c2cc2
thdalstn6352 <thdalstn6352@naver.com>
2021-12-15 20:43:39 +0900

    login page styling

--- Javascript Study/Momentum/js/greeting.js
+++ Javascript Study/Momentum/js/greeting.js
@@ -1,6 +1,8 @@
-const loginForm = document.querySelector("#login-form");
+const loginForm = document.querySelector("#login");
 const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
+const todo = document.querySelector("#todo");
+// const todo = document.querySelector("#todo-section");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -9,7 +11,7 @@ const saveUsername = localStorage.getItem(USERNAME_KEY);
 function onLoginSubmit(e) {
   e.preventDefault();
   const username = loginInput.value;
-  loginForm.classList.add(HIDDEN_CLASSNAME);
+  loginForm.className = HIDDEN_CLASSNAME;
   greeting.classList.remove(HIDDEN_CLASSNAME);
   localStorage.setItem(USERNAME_KEY, username);
   showGreetingMsg(username);
@@ -17,11 +19,13 @@ function onLoginSubmit(e) {
 
 function showGreetingMsg(username) {
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  todo.className = "todo";
+  // todo.classList.remove(HIDDEN_CLASSNAME);
   greeting.innerText = `Welcome ${username}`;
 }
 
 if (saveUsername === null) {
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.className = "login";
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   showGreetingMsg(saveUsername);
