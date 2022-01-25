34a39755d158ef752609f3faf57d1a82c282999d
thdalstn6352 <thdalstn6352@naver.com>
2022-01-25 22:40:15 +0900

    Change the variable name

--- React Study/react-moviepedia/src/api.js
+++ React Study/react-moviepedia/src/api.js
@@ -1,50 +1,50 @@
-const BASE_URL = 'https://learn.codeit.kr/api';
+const BASE_URL = "https://learn.codeit.kr/api";
 
 export async function getReviews({
-  order = 'createdAt',
+  order = "createdAt",
   offset = 0,
   limit = 6,
 }) {
   const query = `order=${order}&offset=${offset}&limit=${limit}`;
   const response = await fetch(`${BASE_URL}/film-reviews?${query}`);
   if (!response.ok) {
-    throw new Error('리뷰를 불러오는데 실패했습니다');
+    throw new Error("리뷰를 불러오는데 실패했습니다");
   }
-  const body = await response.json();
-  return body;
+  const data = await response.json();
+  return data;
 }
 
 export async function createReview(formData) {
   const response = await fetch(`${BASE_URL}/film-reviews`, {
-    method: 'POST',
+    method: "POST",
     body: formData,
   });
   if (!response.ok) {
-    throw new Error('리뷰를 생성하는데 실패했습니다.');
+    throw new Error("리뷰를 생성하는데 실패했습니다.");
   }
-  const body = await response.json();
-  return body;
+  const data = await response.json();
+  return data;
 }
 
 export async function updateReview(id, formData) {
   const response = await fetch(`${BASE_URL}/film-reviews/${id}`, {
-    method: 'PUT',
+    method: "PUT",
     body: formData,
   });
   if (!response.ok) {
-    throw new Error('리뷰를 수정하는데 실패했습니다.');
+    throw new Error("리뷰를 수정하는데 실패했습니다.");
   }
-  const body = await response.json();
-  return body;
+  const data = await response.json();
+  return data;
 }
 
 export async function deleteReview(id) {
   const response = await fetch(`${BASE_URL}/film-reviews/${id}`, {
-    method: 'DELETE',
+    method: "DELETE",
   });
   if (!response.ok) {
-    throw new Error('리뷰를 삭제하는데 실패했습니다.');
+    throw new Error("리뷰를 삭제하는데 실패했습니다.");
   }
-  const body = await response.json();
-  return body;
+  const data = await response.json();
+  return data;
 }
